350a163260ad7625e0b85f1130c6b178e6638a72
mlblake105 <mlblake105@gmail.com>
2022-03-06 14:48:28 -0600

    Challenge

--- scraping.py
+++ scraping.py
@@ -18,7 +18,8 @@ def scrape_all():
           "news_paragraph": news_paragraph,
           "featured_image": featured_image(browser),
           "facts": mars_facts(),
-          "last_modified": dt.datetime.now()
+          "last_modified": dt.datetime.now(),
+          "hemispheres": get_hemispheres(browser)
     }
     
     # Stop webdriver and return data
@@ -93,6 +94,29 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def get_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range (4):
+        browser.find_by_css("a.product-item img")[i].click()
+        image_url=browser.find_by_text("Sample").first["href"]
+        title=browser.find_by_css("h2.title").first.text
+        hemisphere_image_urls.append({
+            "title":title, 
+            "image_url":image_url
+        })
+        browser.back()
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
 
 
 if __name__ == "__main__":
